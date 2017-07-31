7574022616fdbae5299ce7f4f7b8f8de5ac56bd3
Emmy Emme <masteremmy.js@gmail.com>
2017-07-31 02:12:21 -0400

    Built out clozeCard.js and tested txt file in CLI

--- basicCard.js
+++ basicCard.js
@@ -1,7 +1,7 @@
 // // Depend on Node module to export a constructor for creating the BASIC flashcards 
 // module.exports = basicCard; 
 
-// Require NPM Packages
+// // Require NPM Packages
 
 const fs = require("fs"); 
 const inquirer = require("inquirer"); 
@@ -21,7 +21,7 @@ function BasicCardCreation (front, back) {
 	this.logCard = function () {
 
 	// Create object wherein info will be stored 
-	let writeCard = "Front: " + this.front + "Back: " + this.back + "\n";
+	let writeCard = "Front: " + this.front + "\n" + "Back: " + this.back + "\n";
 
 	// Append user info to the txt file so that it stores 
 	fs.appendFile("basicCardText.txt", writeCard);
@@ -45,7 +45,7 @@ function createCard() {
     ]).then(function(answers) {
  // Define new instance of this object that accepts two arguments from user: front and back 
  // Since it is NEW, name it something different from the OG constructor function obvs 
-        var card = new CreateBasicCard(answers.front, answers.back);
+        var card = new BasicCardCreation(answers.front, answers.back);
         card.logCard();
         // If user wants to create more cards, 
         // call upon recursive function until meeseeks fulfills the obligations  
--- clozeCard.js
+++ clozeCard.js
@@ -1,9 +1,70 @@
 // // Depend on Node module to export a constructor for creating the CLOZE-DELETION flashcards 
 // module.exports = clozeCard; 
 
-// Require NPM Packages
+// //Require NPM Packages
 
 const fs = require("fs"); 
 const inquirer = require("inquirer"); 
 
+// Create constructor that accepts two arguments: Text and Cloze-Deletion
 
+function ClozeCardCreation (text, cloze) {
+	// Accept the argument cloze for Cloze-Deletion and declare it as cardCloze
+	this.cardCloze = cloze, 
+	// Accept the argument text for text and declare it as .cardText 
+	this.cardText = text, 
+	// Stage the whole text for deletion 
+	this.whole = "{{c::" + this.cardCloze + "}}" + " " + this.cardText; 
+    // Log the deleted text and  
+    this.clozeLogger = function() {
+        console.log("Cloze " + this.cardCloze + " " + "Text: " + this.cardText);
+    }
+    // Write the card and access the file system to write to a txt file 
+    this.cardWriter = function() {
+        var writeCard = this.whole + "\r\n";
+
+        fs.appendFile("cardsCloze.txt", writeCard); 
+    }
+    // Declare cloze as the part of the text that has been deleted 
+    this.displayClozeDeleted = function() {
+    	let cloze = this.whole.replace(this.cardCloze,"...");
+    	return cloze;
+    }
+};
+// Prompt the user with the questions regarding the Cloze-Deletion part of the text 
+// and THEN since we care about their reply, accept the "Answers" callback
+function createCard() {
+    inquirer.prompt([
+        {name: "cloze",
+        message: "Enter text for cloze side: "}, 
+        {name: "text",
+        message: "Enter text for whole flashcard "}, 
+        {type: "confirm",
+        message: "Display cloze deleted flashcard?",
+        name: "display",
+        default: true}, 
+        {type: "confirm",
+        message: "Do you want to add another card?",
+        name: "continue",
+        default: true
+        }
+// and THEN since we care about their reply, accept the "Answers" callback
+// also declare a variable that produces NEW card and write  
+    ]).then(function(answers) {
+        //create NEW 
+        var card = new ClozeCardCreation(answers.text, answers.cloze);
+        card.cardWriter();
+        // if user is finished makign cards then log it if not then allow them to create another
+        // card that the 
+        if(answers.display === true)
+        	console.log(card.displayClozeDeleted());
+
+        if(answers.continue === true)
+            createCard();
+        else
+            console.log("cardsCloze.txt")
+    });
+}
+//call the original constructor function
+createCard();
+}
